16e73774acb6efcf24bcc2164d5bee750134d0c1
unknown <madhavmishra083@gmail.com>
2025-08-19 17:02:01 +0530

    Working on the about section has been started

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import codingIMG from '/coding.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +17,9 @@ function App() {
           </ul>
         </div>
       </nav>
+      <div>
+        <img src={codingIMG} alt="" srcset="" className='flex justify-start items-start w-64 h-auto inline-block'/>
+      </div>
     </>
   )
 }
