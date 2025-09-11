1a1efedcc0eaa0895db146693a2cf1a9f426bc9e
unknown <madhavmishra083@gmail.com>
2025-09-11 20:01:41 +0530

    Functionality work has been started

---
+++ src/res.jsx
@@ -0,0 +1,13 @@
+import App from './src/App.jsx';
+import calc from './calc.js';
+import {useState} from 'react';
+function Result() {
+    const [count,setCount]=useState(0);
+  return (
+    <div>
+        Hello
+    </div>
+
+    )}
+
+export default Result;
